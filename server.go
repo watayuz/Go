91e27859e3074e9c61607a0664dea347602c806d
watayuz <w-desu@i.softbank.jp>
2016-10-29 01:05:46 +0900

    create server.go

--- server.go
+++ server.go
@@ -2,30 +2,16 @@ package main
 
 import (
 	"net/http"
-	"log"
-	"html/template"
+	"fmt"
 )
 
-type WeatherData struct {
-	Name string 'json:"name"'
-	Main struct {
-		Kelvin float64 'json:temp'
-		} 'json:"Main"'
-	}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello, world")
+}
 
-	func handler(w http.ResponseWriter, r *http.Request) {
-		temp := template.New("fieldname example")
-		temp, _ = temp.Parse("hello {{.UserName}}")
-		p := IndexPage{UserName : "in user name"}
-		temp.Execute(w, p)
-	}
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Printf("Start Go Server localhost:8080")
 
-	func main() {
-		http.HandleFunc("/", handler)
-		http>HandleFunc("/api/test", func(w http.ResponseWriter, r *http.Request){
-			
-		})
-		log.Printf("Start Go Server localhost:8080")
-
-		http.ListenAndServe(":8080", nil)
-	}
+	http.ListenAndServe(":8080", nil)
+}
