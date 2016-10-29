5e79bab7bf536a2aa744e4ebb74666e76dc7418e
watayuz <w-desu@i.softbank.jp>
2016-10-29 19:52:33 +0900

    keyを別ファイルに分けた

--- chat/chat.go
+++ chat/chat.go
@@ -1,51 +1,48 @@
 package main
 
 import (
-"bytes"
-"net/http"
-"fmt"
-"io/ioutil"
-)
+	"bytes"
+	"net/http"
+	"fmt"
+	"io/ioutil"
 
-type SettingJson struct {
-	key string
-	message string
-}
+	// config package path:"config/config.go"
+	"./config"
+)
 
 func main() {
 
 	url_string := "https://chatbot-api.userlocal.jp/api/chat?"
-	key := ""
+	key_string := config.Key
 
-	error := HttpPost(url_string, key, "こんにちわ")
+	error := HttpPost(url_string, key_string, "こんにちわ")
 
 	if error != nil {
 		fmt.Println(error)
 	}
-
 }
 
-func HttpPost(url_string, key_string string, message_string string) error {
+func HttpPost(url_string, key_string, message_string string) error {
 	jsonStr := []byte(`{"key":"` + key_string + `","message":"` + message_string + `"}`)
 
 	req, error := http.NewRequest(
 		"POST",
 		url_string,
 		bytes.NewBuffer(jsonStr),
-	)
-if error != nil {
-	panic(error)
-}
+		)
+	if error != nil {
+		panic(error)
+	}
 
-req.Header.Set("Content-Type", "application/json")
-client := &http.Client{}
-resp, error := client.Do(req)
-defer resp.Body.Close()
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, error := client.Do(req)
+	defer resp.Body.Close()
 
-fmt.Println("response Status:", resp.Status)
-fmt.Println("response Headers:", resp.Header)
-body, _ := ioutil.ReadAll(resp.Body)
-fmt.Println("response Body:", string(body))
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 
-return error
+	return error
 }
